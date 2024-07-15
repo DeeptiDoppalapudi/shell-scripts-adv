ca1e91569b7d2eabb2c3ac4f317be980c0319026
Your Name <you@example.com>
2024-07-15 11:52:39 +0530

    Hello

---
+++ 11-functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing Git"
