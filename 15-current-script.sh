05689cff6c8047b7a90f838118576d8c4d9dcddd
Your Name <you@example.com>
2024-07-18 16:53:59 +0530

    Hello

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -4,7 +4,7 @@ COURSE="DevOps from current script"
 echo "Before calling other script : $COURSE"
 echo "Process ID of current script: $$"
 
-#./16-other-script.sh
+./16-other-script.sh
 
 echo "After calling other script : $COURSE"
 echo "Process ID of current script: $$"
