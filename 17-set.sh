5c2acd4c271379e1501eeec2425aa6d378be33f9
Your Name <you@example.com>
2024-07-19 14:35:25 +0530

    Hello

---
+++ 17-set.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [$USERID -ne 0]
+then 
+echo "Please run this script with root access"
+exit 1
+else
+echo "You are super user"
+fi
+
+dnf install mysqlly -y
+dnf install git -y
+
+echo "Is Script proceeding"
+
+
