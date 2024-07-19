899d942f25a165785a713f6745a13dcabddd77a3
Your Name <you@example.com>
2024-07-19 19:14:14 +0530

    Hello

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
+then 
+echo -e "$G source directory exists $N"
+else
+echo -e "$R Make sure $SOURCE_DIRECTORY exists $N"
+exit1
+fi
+
+
+
+
